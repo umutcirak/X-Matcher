1b90e07cb45a672daf452172fe18dd2c5a7900bf
umutcirak <umut.cirak@ozu.edu.tr>
2022-05-29 00:50:08 +0300

    Singleton Score Keeper script.

---
+++ Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    
+    [SerializeField] int scoreIncrease = 100;
+    public int currentScore;
+
+    static ScoreKeeper instance;
+
+    void Awake()
+    {
+        ManageSingleton();
+    }
+
+    void ManageSingleton()
+    {
+
+        if (instance != null)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+
+    public void IncrementScore(int matchSize)
+    {
+        int bonusMultiplier = (int)matchSize / 2;
+        currentScore += matchSize * bonusMultiplier * scoreIncrease;
+    }
+
+
+}
