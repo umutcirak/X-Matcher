bf4a768fde31572598c45389cb1c07a7495e592c
umutcirak <umut.cirak@ozu.edu.tr>
2022-05-29 00:52:45 +0300

    Grid attributes script.

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public int gridSize;
+
+    void Awake()
+    {
+        ManageSingleton();
+    }
+   
+
+    void Start()
+    {
+        gridSize = 5;
+    }
+
+    void ManageSingleton()
+    {
+        int instanceCount = FindObjectsOfType(GetType()).Length;
+        if (instanceCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+
+}
