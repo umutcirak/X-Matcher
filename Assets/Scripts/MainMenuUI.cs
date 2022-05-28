921a266d75b43f06d93f608f50ac46f33ba137f9
umutcirak <umut.cirak@ozu.edu.tr>
2022-05-29 00:52:03 +0300

    Main Menu UI.

---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{    
+   
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+
+
+
+
+
+}
