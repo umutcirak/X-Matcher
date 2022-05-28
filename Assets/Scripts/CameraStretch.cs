eddea51552ad5b008a34f8d4f3d67c53869ed380
umutcirak <umut.cirak@ozu.edu.tr>
2022-05-29 00:53:28 +0300

    Camera Positioner script.

---
+++ Assets/Scripts/CameraStretch.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraStretch : MonoBehaviour
+{   
+    
+    Camera mainCamera;
+    Board board;
+
+
+    void Awake()
+    {
+        mainCamera = Camera.main;
+        board = FindObjectOfType<Board>();
+
+    }
+
+    void Start()
+    {
+        StretchCamera();
+    }   
+    
+
+    void StretchCamera()
+    {
+        // posIndex = (Grid Size - 1) * 0.5
+        float size = board.gridSize * 0.55f;
+        float posIndex = (board.gridSize - 1) * 0.5f;
+
+        float shiftLeft = board.gridSize / 2.5f;
+
+        Vector3 pos = new Vector3(posIndex - shiftLeft, posIndex, -1f);
+
+        mainCamera.transform.position = pos;
+
+        mainCamera.orthographicSize = size;    
+                
+    }
+
+
+
+}
