2f9cfec74d7c179dda523b3e575b758d7b7ddc3d
umutcirak <umut.cirak@ozu.edu.tr>
2022-05-29 00:54:17 +0300

    End Scene UI.

---
+++ Assets/Scripts/EndUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class EndUI : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI scoreText;
+            
+    void Start()
+    {
+        scoreText.text = FindObjectOfType<ScoreKeeper>().currentScore.ToString();
+    }    
+
+    public void OpenMainMenu()
+    {
+        FindObjectOfType<ScoreKeeper>().currentScore = 0;
+        FindObjectOfType<GridManager>().gridSize = 5;
+        SceneManager.LoadScene(0);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+
+
+
+}
