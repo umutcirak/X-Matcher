67b4cd40041d0fb3442ba3207cb74e034f140a2c
umutcirak <umut.cirak@ozu.edu.tr>
2022-05-29 00:54:02 +0300

    SFX added.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    
+    [SerializeField] AudioClip matchClip;
+    [SerializeField][Range(0,1f)] float matchClipCVolume;
+
+   
+    public void PlayMatchClip()
+    {
+        AudioSource.PlayClipAtPoint(matchClip, Camera.main.transform.position, matchClipCVolume);
+    }
+    
+
+}
